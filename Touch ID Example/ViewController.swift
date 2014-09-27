32c3170d802abfbb0c548186d836564dfec88cc5
Adam Leonard <adamjamesleonard@gmail.com>
2014-09-27 16:49:28 -0400

    Initial Commit

---
+++ Touch ID Example/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Touch ID Example
+//
+//  Created by Adam Leonard on 9/27/14.
+//  Copyright (c) 2014 com.swiftcast.tv. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
