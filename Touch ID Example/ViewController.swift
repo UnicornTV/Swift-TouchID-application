9adda46a6b1392428ff6dcb5db8ee4d158242712
Adam Leonard <adamjamesleonard@gmail.com>
2014-09-27 20:23:01 -0400

    Initial application

--- Touch ID Example/ViewController.swift
+++ Touch ID Example/ViewController.swift
@@ -7,9 +7,12 @@
 //
 
 import UIKit
+import LocalAuthentication
 
 class ViewController: UIViewController {
 
+    @IBOutlet var messageLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +22,5 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
